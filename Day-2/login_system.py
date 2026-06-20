3326e429a30c038889c6e84b90ce0f26b211572c
senpaihamza26-lab <senpaihamza26@gmail.com>
2026-06-20 10:59:41 +0530

    Added login_system.py

---
+++ Day-2/login_system.py
@@ -0,0 +1,8 @@
+username = input("enter Username : ")
+password = input("Enter Password : ")
+
+if username == "Your username " and password == "your password" :
+    print("Log in sucessfull ")
+
+else :
+    print("Invalid Username and Password")
